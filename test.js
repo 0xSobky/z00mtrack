9cfbe73c9c9b89fec42328bb21d829eebbafd699
Ahmed Elsobky <0xsobky@gmail.com>
2018-02-17 15:35:09 +0200

    Change indentation to 4 spaces

--- test.js
+++ test.js
@@ -7,20 +7,19 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(4);
+    t.plan(4);
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
+    // Wait until the server is ready
+    child.stdout.on('data', _ => {
+        // Make a request to our app
+        request('http://127.0.0.1:5000', (error, response, body) => {
+            // stop the server
+            child.kill();
 
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-
+            // No error
+            t.false(error);
+            // Successful response
+            t.equal(response.statusCode, 200);
+        });
     });
-  });
 });
